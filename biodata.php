3ee7d4dfbe561e0242004bef9c31db8fe592ce61
MFakhruddin <alungmuhammad200@gmail.com>
2022-03-29 19:32:37 +0700

    biodata.php

---
+++ biodata.php
@@ -0,0 +1,21 @@
+<?php
+class Biodata extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>PERKENALKAN </h1>";
+        echo " <p> BIODATA DIRI SAYA</p>";
+        echo " <p> NAMA                : MUHAMMAD FAKHRUDDIN</p>";
+        echo " NIM                     : 17190322";
+        echo " <p> KELAS               : 17.6A.25 </p>";
+        echo " Fakultas                : Teknologi INformasi";
+        echo " <p> UNIVERSITAS         : BINA SARANA INFORMATIKA</p>";
+        echo " JENIS KELAMIN           : LAKI-LAKI";
+        echo " <p>TEMPAT,TANGGAL LAHIR : JAKARTA, 20 OKTOBER 2001</p>";
+        echo " GOLONGAN DARAH          : A";
+        echo " <p>ALAMAT               : JL DUKU II/2</p>";
+        echo " HOBBY                   : MAEN LAYANGAN";
+        echo " <p> E-MAIL              : alungmuhammad200@gmail.com</p>";
+        echo " NO.HP                   : 081286162151";
+    }
+}
