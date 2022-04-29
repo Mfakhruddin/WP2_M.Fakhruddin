62380441215c8c2a4d8d09b28168f7dd0b86a531
Mfakhruddin <101697965+Mfakhruddin@users.noreply.github.com>
2022-04-29 22:57:22 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Muhammad</li>
+ <li>Nama Belakang : fakhruddin</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Dukuh II/2 </li>
+ </ul>
+
+ <h4>Tempat Lahir, Tanggal Lahir</h4>
+ <ul type="none">
+ <li>Jakarta, 20 Oktober 2001</li>
+ </ul>
+ <h4>MY HOBBY</h4>
+ <ul type="square">
+ <li>Bikin Talikama </li>
+ <li>Mancing</li>
+ </ul>
+</section>
